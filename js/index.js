ce43d55474975009985629c305d7287324853472
Prasannak <144816994+prasannauixdesigner@users.noreply.github.com>
2024-02-06 08:36:06 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+//Smooth Scrolling effect
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("nav a");
+
+  links.forEach((link) => {
+    link.addEventListener("click", smoothScroll);
+  });
+
+  function smoothScroll(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scroll({
+      top: targetElement.offsetTop,
+      behavior: "smooth",
+    });
+  }
+});
+
+//cursor animation
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.querySelector(".custom-cursor");
+
+  document.addEventListener("mousemove", function (e) {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+  });
+});
+
+//Data animate
+document.addEventListener("DOMContentLoaded", function () {
+  const animatedElements = document.querySelectorAll("[data-animate]");
+
+  function checkElements() {
+    animatedElements.forEach((element) => {
+      if (isElementInViewport(element)) {
+        element.classList.add("animate");
+      }
+    });
+  }
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Check elements on initial load
+  checkElements();
+
+  // Check elements on scroll
+  window.addEventListener("scroll", checkElements);
+});
+
+//hide after my-motto section
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the number of sections
+  var numberOfSections = document.querySelectorAll("section").length;
+
+  // Check if there are at least 4 sections
+  if (numberOfSections >= 4) {
+    // Add the class to hide social-connect
+    document.body.classList.add("hide-social");
+  }
+});
